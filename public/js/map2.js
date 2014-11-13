ac4fde771794e07240f480a982a8cecde1a46477
Youjin Shin <meggonagul@gmail.com>
2014-11-13 18:31:56 -0500

    edit caption

--- public/js/map2.js
+++ public/js/map2.js
@@ -10,9 +10,9 @@ var projection = d3.geo.equirectangular()
     // .translate([width/2+160, 440])
     // .precision(0.02); //.1
 
-    .scale(220) // 340, 270
+    .scale(230) // 340, 270
     .rotate([160, 0]) // 160,0
-    .translate([1070, 430])
+    .translate([1040, 430])
     .precision(0.02); //.1
 
 var path = d3.geo.path()
